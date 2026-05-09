2cf6a7c42739f99592420a88421e3b8ae0d8fd00
Anshika <anshikaclg2001@gmail.com>
2026-05-09 15:30:48 -0500

    added streak counter

--- src/HabitList.jsx
+++ src/HabitList.jsx
@@ -1,10 +1,26 @@
 function HabitList({habits, toggle}){
+    function streakCalc(dates){
+        let streak = 0
+        let currentDate = new Date()
+        if (dates.length === 0){
+            return 0
+        }
+        const sorted = [...dates].sort((a,b) => new Date(b) - new Date(a))
+        while(sorted.includes(currentDate.toDateString())){
+            streak += 1
+            currentDate = new Date(currentDate - 86400000)
+        }
+        return streak
+    }
     return(
         <div>
             <h1>Habit List Works</h1>
             <ul>
                 {habits.map(habit => 
-                <li key={habit.id}>{habit.name} <button onClick={() => toggle(habit.id)}>Done</button> </li>
+                <li key={habit.id}>{habit.name} 
+                <button onClick={() => toggle(habit.id)}>Done</button> 
+                🔥 {streakCalc(habit.completedDates)}
+                </li>
                 )}
             </ul>
         </div>
