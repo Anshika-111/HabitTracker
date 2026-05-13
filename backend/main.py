b0100b7a0213a6059170be6b45b5a7326e614aa8
Anshika <anshikaclg2001@gmail.com>
2026-05-13 12:03:56 -0500

    connected FastAPI to supabase

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from supabase import create_client
+import os
+
+load_dotenv()
+
+supabase = create_client(
+    os.getenv("SUPABASE_URL"),
+    os.getenv("SUPABASE_KEY")
+)
 
 app = FastAPI()
 
@@ -13,23 +23,19 @@ app.add_middleware(
 
 @app.get("/habits")
 def read_root():
-    return [
-    {"id": 1, "name": "drink water"},
-    {"id": 2, "name": "exercise"}
-    ]
+    return supabase.table("habits").select("*").execute().data
 
 class HabitCreate(BaseModel):
     name: str
 
 @app.post("/habits")
 def create_root(habit: HabitCreate):
-    return {"id": 43, "name": habit.name}
+    return supabase.table("habits").insert({"name": habit.name, "user_id": "temp-user"}).execute().data
 
 @app.put("/habits/{habit_id}")
-def update_habit(habit_id: int, habit: HabitCreate):
-    return {"id": habit_id, "name": habit.name}
+def update_habit(habit_id: str, habit: HabitCreate):
+    return supabase.table("habits").update({"name": habit.name}).eq("id", habit_id).execute().data
 
 @app.delete("/habits/{habit_id}")
-def delete_habit(habit_id: int):
-    return {"message": "Habit was deleted!"}
-
+def delete_habit(habit_id: str):
+    return supabase.table("habits").delete().eq("id", habit_id).execute().data
